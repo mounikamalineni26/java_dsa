4a6ce81fb5feece0e2cddb1ff349bc61e96b77a0
Mounika Malineni <mounikamalineni63@gmail.com>
2026-02-28 15:26:43 +0530

    Rename Main class to DFS

--- Graphs/DFS.java
+++ Graphs/DFS.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Main{ 
+public class DFS{ 
     public static void dfs(int node, boolean[] visited, ArrayList<ArrayList<Integer>> graph) {
         visited[node] = true;
         System.out.print(node + " ");
